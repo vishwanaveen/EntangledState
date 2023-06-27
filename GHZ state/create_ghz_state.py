a1a5ea34856befc90de24aab9c5791ffae7549ff
vishwanaveen <83831172+vishwanaveen@users.noreply.github.com>
2023-06-28 03:42:41 +0530

    Create create_ghz_state.py
    ghz gate

---
+++ GHZ state/create_ghz_state.py
@@ -0,0 +1,12 @@
+
+
+def create_ghz_state(num_qubits):
+    circuit = QuantumCircuit(num_qubits)
+    circuit.h(0)  # Apply a Hadamard gate to the first qubit
+
+    for i in range(1, num_qubits):
+        circuit.cx(0, i)  # Apply a CNOT gate from the first qubit to the remaining qubits
+
+    return circuit
+
+
